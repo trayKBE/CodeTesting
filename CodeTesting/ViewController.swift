a1d4e17356a3369f1e668861bad4f3d915ea4a9b
蓝凌 <lanling@lanlings-MacBook-Pro.local>
2015-08-08 08:49:45 +0800

    Initial Commit

---
+++ CodeTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeTesting
+//
+//  Created by 蓝凌 on 8/8/15.
+//  Copyright (c) 2015 Neal Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
